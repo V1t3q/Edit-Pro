30d9c639774a6d4428ec1c04841a4b8b2fb0af06
V1t3q <trhlik.vit450@gmail.com>
2024-05-22 01:47:07 +0200

    1version

---
+++ assets/scripts/modal.js
@@ -0,0 +1,59 @@
+/*let myButton = document.querySelector('#myBtn')
+let modalContent = document.querySelector('.modal-' +
+    'content');
+let backdrop = document.querySelector('.backdrop');
+let close = document.querySelector('.close');
+
+myButton.addEventListener('click', openModal);
+close.addEventListener('click', closeModal);
+document.addEventListener('click', function (element){
+
+    if (element.target === backdrop) {
+        closeModal();
+    }
+});
+
+document.addEventListener('keydown', function (key){
+    if(key.key === 'Escape') {
+        closeModal()
+    }
+});
+
+function openModal() {
+    console.log("open modal")
+    backdrop.style.display = 'block';
+}
+
+function closeModal(){
+    console.log("close modal")
+    backdrop.style.display = 'none';
+}
+
+ */
+let myButton = document.querySelector('#myBtn');
+let modal = document.querySelector('.modal');
+let close = document.querySelector('.close');
+
+myButton.addEventListener('click', openModal);
+close.addEventListener('click', closeModal);
+document.addEventListener('click', function (event){
+    if (event.target === modal) {
+        closeModal();
+    }
+});
+
+document.addEventListener('keydown', function (event){
+    if(event.key === 'Escape') {
+        closeModal();
+    }
+});
+
+function openModal() {
+    console.log("open modal");
+    modal.style.display = 'block';
+}
+
+function closeModal(){
+    console.log("close modal");
+    modal.style.display = 'none';
+}
